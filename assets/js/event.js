849b31d311d1146f2217073e1b30752c1bb2fba1
Edward J <edjeong99@gmail.com>
2020-07-22 11:07:07 -0500

    implement responsive navbar

--- assets/js/event.js
+++ assets/js/event.js
@@ -2,24 +2,17 @@
 
 $(document).ready(function () {
   $('#homeLink').click(function () {
+    toggleMenu();
     displayList();
   });
   $('#entryLink').click(function () {
+    toggleMenu();
     displayEntry();
   });
 });
 
-// toggle displaying menu when menu icon is clicked.
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
-
-navBarToggle.addEventListener('click', function () {
-  console.log('navBar toggle clicked');
-  mainNav.classList.toggle('active');
-});
-
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
+function toggleMenu() {
   var x = document.getElementById('myTopnav');
   if (x.className === 'topnav') {
     x.className += ' responsive';
