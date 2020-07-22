079539b2756c18ec6b50c03c5a09d8551df60ebe
Edward J <edjeong99@gmail.com>
2020-07-21 19:22:58 -0500

    start working on navbar mobile

--- assets/js/event.js
+++ assets/js/event.js
@@ -8,3 +8,12 @@ $(document).ready(function () {
     displayEntry();
   });
 });
+
+// toggle displaying menu when menu icon is clicked.
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function () {
+  console.log('navBar toggle clicked');
+  mainNav.classList.toggle('active');
+});
