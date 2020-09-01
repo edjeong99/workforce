100bb9152d5907acb5e511e4692c8e1d7c8ec13f
Edward J <edjeong99@gmail.com>
2020-09-01 16:54:08 -0500

    commenting out sw.js so I can use workbox now

--- sw.js
+++ sw.js
@@ -9,36 +9,36 @@ var appShellFiles = ['index.html'];
 // var contentToCache = appShellFiles.concat(gamesImages);
 
 // Installing Service Worker
-self.addEventListener('install', function (e) {
-  console.log('[Service Worker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-      console.log('[Service Worker] Caching all: app shell and content');
-      console.log(appShellFiles);
-      return cache.addAll(appShellFiles);
-    })
-  );
-});
+// self.addEventListener('install', function (e) {
+//   console.log('[Service Worker] Install');
+//   e.waitUntil(
+//     caches.open(cacheName).then(function (cache) {
+//       console.log('[Service Worker] Caching all: app shell and content');
+//       console.log(appShellFiles);
+//       return cache.addAll(appShellFiles);
+//     })
+//   );
+// });
 
 // Fetching content using Service Worker
-self.addEventListener('fetch', function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (r) {
-      console.log('[Service Worker] resource in cache: ' + e.request.url);
-      return (
-        // below code used to be ||.  it meant use cache first.
-        // I changed to && to get the network update first.
-        r &&
-        fetch(e.request).then(function (response) {
-          return caches.open(cacheName).then(function (cache) {
-            console.log(
-              '[Service Worker] Caching new resource: ' + e.request.url
-            );
-            cache.put(e.request, response.clone());
-            return response;
-          });
-        })
-      );
-    })
-  );
-});
+// self.addEventListener('fetch', function (e) {
+//   e.respondWith(
+//     caches.match(e.request).then(function (r) {
+//       console.log('[Service Worker] resource in cache: ' + e.request.url);
+//       return (
+//         // below code used to be ||.  it meant use cache first.
+//         // I changed to && to get the network update first.
+//         r &&
+//         fetch(e.request).then(function (response) {
+//           return caches.open(cacheName).then(function (cache) {
+//             console.log(
+//               '[Service Worker] Caching new resource: ' + e.request.url
+//             );
+//             cache.put(e.request, response.clone());
+//             return response;
+//           });
+//         })
+//       );
+//     })
+//   );
+// });
